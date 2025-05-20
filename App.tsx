c4e209707aadb7cca06d3ba3ebffc4103b89d307
Gules79 <mgules5@gmail.com>
2025-05-20 11:17:28 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bom dia, vamos inter!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
